ba79eed136c7377b952d9d553cdfead7764838d1
Federico Rizzardini <federico.rizzardini@gmail.com>
2014-07-03 22:50:59 +0200

    Added Ex2 Assignment 1

--- Assignment1/Ex2/ass-2.py
+++ Assignment1/Ex2/ass-2.py
@@ -9,19 +9,17 @@ list = input.readlines()
 firstLine = list[0].split()
 numberOfVertices = int(firstLine[0])
 
-print('Number of Nodes %d' % numberOfVertices)
-
 list = list[1:len(list)]
 edges = []
 
 for line in list:
-        u , v , cost = [int(x) for x in line.split()]
-        edges.append((u, v, cost))
+    u , v , cost = [int(x) for x in line.split()]
+    edges.append((u, v, cost))
+    edges.append((v, u, cost))
 
 criterion = lambda(u, v, cost): cost
 
 edges.sort(key = criterion)
-print(edges)
 
 vertices = []
 T = []
@@ -29,6 +27,15 @@ T = []
 vertices.append(edges[0][0])
 
 while len(vertices) != numberOfVertices:
-	
+	for element in edges:
+		if element[0] in vertices and element[1] not in vertices:
+			T.append(element)
+			vertices.append(element[1])
+			break
 
+total = 0
 
+for line in T:
+	total += line[2]
+
+print(total)
